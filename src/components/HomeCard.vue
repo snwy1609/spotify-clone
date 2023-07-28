163f6232b880965463383c4b3fa9935e8cff6ef8
Jean Esteban <jean@pop-os.localdomain>
2023-07-28 20:22:43 +0800

    first commit on-going song row

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <RouterLink to="library">
+        <div class="bg-[#111111] p-4 rounded-md m-2 hover:bg-[#252525] cursor-pointer">
+            <img class="rounded-md" :src="image">
+            <div class="text-white pt-4 font-semibold text-[17px]">{{ title }}</div>
+            <div class="text-gray-400 pt-1 pb-3 text-[14px]">{{ subtitle }}</div>
+      
+        </div>
+    </RouterLink>
+  
+</template>
+
+<script setup>
+    import { toRefs } from 'vue'
+    import { RouterLink } from 'vue-router';
+
+    const props = defineProps({
+        image: String,
+        title: String,
+        subtitle: String
+    })
+
+    const {image , title, subtitle} = toRefs(props)
+
+  
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SongRow.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <li class="flex items-center justify-between rounded-md hover:bg-[#2A2929]"
+        @mouseenter="isHover = true"
+        @mouseleave="isHover = false">
+
+        <div class="flex items-center w-full py-1.5">
+            
+        </div>
+
+    </li>
+  
+</template>
+
+<script setup>
+
+import { ref, toRefs, onMounted } from 'vue'
+import Heart from 'vue-material-design-icons/Heart.vue';
+import Play from 'vue-material-design-icons/Play.vue';
+import Pause from 'vue-material-design-icons/Pause.vue';
+
+let isHover = ref(false)
+let isTrackTime = ref(null)
+
+const props = defineProps({
+    track: Object,
+    artist: Object,
+    index: Number,
+})
+
+const { track, artist, index} = toRefs(props)
+</script>
+
